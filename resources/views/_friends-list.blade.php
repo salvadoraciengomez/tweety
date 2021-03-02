069b3803d14c0c5bebd59e3fe380e1eea382a175
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-02 13:51:15 +0100

    Updated views and profile/{user}

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -3,7 +3,7 @@
     @foreach (auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
-                <a href="{{ route('profile', $user) }}" class="flex items-center text-sm"><img src="{{ $user->avatar }}" alt="" class="rounded-full mr-4">{{ $user->name }}</a>
+                <a href="{{ route('profile', $user) }}" class="flex items-center text-sm"><img src="{{ $user->avatar }}" alt="" width="40" height="40" class="rounded-full mr-4">{{ $user->name }}</a>
             </div>
         </li>
     @endforeach
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,8 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>profile page for {{ $user->name }}</h3>
-    <hr>
+    {{-- <h3>profile page for {{ $user->name }}</h3>
+    <hr> --}}
+    <header class="mb-6">
+        <img src="/images/bio.png" alt="" class="mb-2">
+        <div class="flex justify-between items-center">
+            <div>
+                <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
+                <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
+            </div>
+            <div>
+                <a href="" class="rounded-full shadow py-2 px-4 text-black text-xs">Edit Profile</a>
+                <a href="" class="bg-blue-500 rounded-full shadow px-4 py-2 text-white text-xs">Follow Me</a>
+            </div>
+        </div>
+        <img src="{{ $user->avatar }}" alt="" class="rounded-full mr-2 absolute" style="width:150px;top:47%;left:calc(45%);top:215px">
+    </header>
     @include('_timeline',[
         'tweets' => $user->tweets
     ])
