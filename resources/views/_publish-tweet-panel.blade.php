015ac1f59081701c70597dda8b8c60b4c5c0baa4
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-02 10:17:41 +0100

    Frontend creation @vid56 ok

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,10 @@
+<div id="textbox" class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+        <textarea name="body" id="" class="w-full" placeholder="Escribe tu tuit"></textarea>
+        <hr class="my-4">
+        <footer class="flex justify-between">
+            <img src="https://i.pravatar.cc/40" alt="" class="rounded-full mr-2">
+            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweetea</button>
+        </footer>
+    </form>
+</div>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,11 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div id="avatar" class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+    <div id="userinfo">
+        <h5 class="font-bold mb-4">JohnDoe</h5>
+        <p class="text-sm">
+            User tweet content hardcoded no database Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nesciunt, eius voluptate vero ipsum quam aperiam laborum illo! Eos dolorem atque rem eaque non modi vitae sapiente quod perferendis maxime!
+        </p>
+    </div>
+</div>
