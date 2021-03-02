94093fae3871e9455a97a26c6e00a2319d5cb6e1
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-02 13:22:03 +0100

    User profile and sidebar links

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -3,8 +3,7 @@
     @foreach (auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
-                <img src="{{ $user->avatar }}" alt="" class="rounded-full mr-4">
-                {{ $user->name }}
+                <a href="{{ route('profile', $user) }}" class="flex items-center text-sm"><img src="{{ $user->avatar }}" alt="" class="rounded-full mr-4">{{ $user->name }}</a>
             </div>
         </li>
     @endforeach
--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -1,21 +1,9 @@
 <ul>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/">Home</a>
+        <a class="font-bold text-lg mb-4 block" href="{{ route('home') }}">Home</a>
     </li>
 
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/explore">Explore</a>
-    </li>
-
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="">Notifications</a>
-    </li>
-
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="">Messages</a>
-    </li>
-
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="">Bookmarks</a>
+        <a class="font-bold text-lg mb-4 block" href="{{ route('profile', auth()->user())}}">Profile</a>
     </li>
 </ul>
