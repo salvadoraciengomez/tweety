5cded169c977c2216fed2eb2196aa69babaf88bd
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-02 11:06:05 +0100

    Added validation publish-tweet blade

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,7 +1,7 @@
 <div id="textbox" class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
     <form  method="POST" action="/tweets">
         @csrf
-        <textarea name="body" id="" class="w-full" placeholder="Escribe tu tuit" required></textarea>
+        <textarea name="body" id="" class="w-full" placeholder="Escribe tu tuit"></textarea>
         <hr class="my-4">
         <footer class="flex justify-between">
             <img src="{{ auth()->user()->avatar }}" alt="" class="rounded-full mr-2"> 
@@ -9,4 +9,7 @@
             <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweetea</button>
         </footer>
     </form>
+    @error('body')
+        <p class="text-red text-sm">{{ $message }}</p>
+    @enderror
 </div>
