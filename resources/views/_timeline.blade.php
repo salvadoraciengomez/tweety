b5d2da242d621670f8c57a05019b426c21cb38d6
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-02 13:26:41 +0100

    Using _timeline.blade

---
+++ resources/views/_timeline.blade.php
@@ -0,0 +1,5 @@
+<div id="timeline" class="border border-gray-300 rounded-lg">
+    @foreach ( $tweets as $tweet)
+        @include('_tweet')
+    @endforeach
+</div>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    profile page for {{ $user->name }}
+    <h3>profile page for {{ $user->name }}</h3>
+    <hr>
+    @include('_timeline',[
+        'tweets' => $user->tweets
+    ])
 @endsection
