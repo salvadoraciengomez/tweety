f97dd8a12bc79decbd74e97e52d986e2c8ac54d7
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-03 11:15:21 +0100

    ProfileEdit not working through PATCH request

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="{{ $user->path() }}">
+    <form action="{{ $user->path() }}" method="GET">
         @csrf
         @method('PATCH')
 
@@ -47,7 +47,7 @@
 
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password_confirmation">Password Confirmation:</label>
-            <input class="border border-gray-400 p-2 w-full" type="password_confirmation" name="password_confirmation" id="password_confirmation" required>
+            <input class="border border-gray-400 p-2 w-full" type="password" name="password_confirmation" id="password_confirmation" required>
             @error('password_confirmation')
                 <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
             @enderror
