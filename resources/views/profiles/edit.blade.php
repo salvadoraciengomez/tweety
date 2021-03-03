fa4659618f516912268aa6adcf6c3cc976558b79
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-03 10:56:25 +0100

    UpdateProfile edit.blade form working

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -52,5 +52,9 @@
                 <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
             @enderror
         </div>
+
+        <div class="mb-6">
+            <button type="submit">Update</button>
+        </div>
     </form>
 @endsection
