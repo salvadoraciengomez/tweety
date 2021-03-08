990f42dc497a7660cf96958594ae1cb22c716371
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-08 13:50:09 +0100

    Added instanceOf assertion Test

--- tests/Feature/app/TweetTest.php
+++ tests/Feature/app/TweetTest.php
@@ -4,12 +4,24 @@ namespace Tests\Feature\app;
 // Created by php artisan make:test app/TweetTest
 //run at vendor\bin\phpunit.bat
 
+use App\Tweet;
+use App\User;
+use App\Followable;
+use App\DB;
+use App\Http\Controllers\TweetsController;
+use App\Http\Controllers\FollowsController;
+use App\Http\Controllers\ProfilesController;
+use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\TweetsController;
+// use App\Followable;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TweetTest extends TestCase
 {
+    public $tweet;
     /**
      * A basic feature test example.
      *
@@ -20,5 +32,16 @@ class TweetTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+    public function testCreaTuitUser1(){
+
+
+        $tweet=Tweet::create([
+            'user_id' => 1,
+            'body' => 'ejemplodetweet'
+        ]);
+
+        $this->assertInstanceOf(Tweet::class, $tweet);
+
     }
 }
